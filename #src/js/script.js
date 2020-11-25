900c2304f0f03379647196aa88a617cad5b471d7
elmiramm <33201578+elmiramm@users.noreply.github.com>
2020-11-26 00:00:34 +0300

    JS load работает resize нет

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,14 +1,64 @@
-// переход на начало страницы по клику на стрелку вверх
-
 ; (function () {
-  var body = document.querySelector('body');
+  let body = document.querySelector('body');
+
+  window.addEventListener('load', function () {
+
+    const windowInnerHeight = window.innerHeight;
+
+    let arrowUp = document.querySelector('.fa-up-arrow');
+    let scrollTop = window.pageYOffset;
+    let heightOfArrowUp = arrowUp.getBoundingClientRect().top + scrollTop;
+
+    console.log(windowInnerHeight);
+    console.log(heightOfArrowUp);
+
+    let displayNoneOrNot = arrowUp.classList.contains('display--none');
+
+    if (displayNoneOrNot) {
+      if (windowInnerHeight >= heightOfArrowUp) {
+        arrowUp.classList.remove('display--none');
+      }
+    } else {
+      if (windowInnerHeight >= heightOfArrowUp) {
+        arrowUp.classList.add('display--none');
+      }
+    }
+  })
+
+  window.addEventListener('resize', function () {
+
+    const windowInnerHeight = window.innerHeight;
+
+    let arrowUp = document.querySelector('.fa-up-arrow');
+    let scrollTop = window.pageYOffset;
+    let heightOfArrowUp = arrowUp.getBoundingClientRect().top + scrollTop;
+
+    console.log(windowInnerHeight);
+    console.log(heightOfArrowUp);
+
+    let displayNoneOrNot = arrowUp.classList.contains('display--none');
+
+    if (displayNoneOrNot) {
+      if (windowInnerHeight >= heightOfArrowUp) {
+        arrowUp.classList.remove('display--none');
+      }
+    } else {
+      if (windowInnerHeight >= heightOfArrowUp) {
+        arrowUp.classList.add('display--none');
+      }
+    }
+  })
 
   var scroll = function (target) {
     var targetTop = target.getBoundingClientRect().top;
     var scrollTop = window.pageYOffset;
     var targetOffsetTop = targetTop + scrollTop;
 
-    window.scrollTo(0, targetOffsetTop);
+    window.scrollTo(0, targetOffsetTop,
+      {
+        behavior: 'smooth'
+      }
+    );
   }
 
   body.addEventListener('click', function (e) {
